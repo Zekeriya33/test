5f98d1db46c79dada69839a68c7951c5cf86d50d
Zekeriya Bozkurt <bozkurt.zekeriya@gmail.com>
2022-08-15 21:47:39 +0300

    6.C

--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -14,7 +14,7 @@ public class Day2GitGithub {
     Not:  Yukaridaki  iki komutu tek seferde kullandiktan sonra ikinci commit lerim icin sadece git push
     kullaniriz
 
-    Not: git log --oneline commit lerimdeki yani versiyonlarimdaki durumu gosterir
+    6-) git log --oneline --> commit lerimdeki yani versiyonlarimdaki durumu gosterir
 
      */
     public static void main(String[] args) {
